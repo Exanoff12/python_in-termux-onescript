4264eb6fbe146864435baf92c653b9d353bfe70e
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2021-10-13 19:48:58 +0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ pyver=3
 sudo apt update
 sudo apt-get install python$pyver --assume-yes
 mkdir python-termux
-clear
 ls
+echo python$pyver
 echo "installation of python $pyver finished"
 echo "script by Exanoff12"
