f6c6c8e755076420a49eb0f304452f13de24b166
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2021-10-13 20:19:27 +0600

    Create pythonLATEST.sh

---
+++ pythonLATEST.sh
@@ -0,0 +1,9 @@
+apt-get install python --assume-yes
+termux-setup-storage
+cd storage
+cd shared
+mkdir python-termux
+cd ~
+clear
+echo "installation of python latest finished"
+echo "script by Exanoff12"
