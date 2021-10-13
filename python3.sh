3ff846a278b436b26cad0ed6dc3084edf7845f91
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2021-10-13 19:00:32 +0600

    Create python3.sh

---
+++ python3.sh
@@ -0,0 +1,9 @@
+apt-get install python3 --assume-yes
+termux-setup-storage
+cd storage
+cd shared
+mkdir python-termux
+cd ~
+clear
+echo "installation of python 3 finished"
+echo "script by Exanoff12"
